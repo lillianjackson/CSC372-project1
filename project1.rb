75b88e51811e1b6b5bbc150a8675cf4a602b26f4
lillianjackson <109557502+lillianjackson@users.noreply.github.com>
2023-10-05 13:41:53 -0700

    added outline

--- project1.rb
+++ project1.rb
@@ -1,3 +1,14 @@
 #Authors: Lillian Jackson and Prasiddhi Gyawali
 #Purpose: create a rss feed reader in terminal
-#Resource: https://www.codementor.io/projects/web/rss-feed-reader-website-atx32j280x
+#Resources used for reference: https://www.codementor.io/projects/web/rss-feed-reader-website-atx32j280x
+# https://medium.com/@krandles/rss-and-ruby-its-really-simple-a32a8654733a
+
+
+=begin
+this is a basic outline of the project
+
+first we need to get the user to give a url to a website 
+then we need to figureout how to rss into the website to display the title desritption of the website and link to original content
+
+there are a couple of other challenges we could do but we should get the basics done first and see if we have time to implement the rest 
+=end
