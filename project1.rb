24f6c366e27cdaababe28afcf480cabba2b9ac2e
prasiddhigyawali <65633555+prasiddhigyawali@users.noreply.github.com>
2023-10-08 13:37:37 -0700

    trial

--- project1.rb
+++ project1.rb
@@ -12,3 +12,40 @@ then we need to figureout how to rss into the website to display the title desri
 
 there are a couple of other challenges we could do but we should get the basics done first and see if we have time to implement the rest 
 =end
+
+require 'rss'
+require 'open-uri'
+require 'socket'
+
+server = TCPServer.new(1337)
+
+loop do
+    client = server.accept
+
+# client.puts "What's your name?"
+# input = client.gets
+# puts "Received #{input.chomp} from a client socket on 1337"
+# client.puts "Hi, #{input.chomp}! You've successfully connected to the server socket."
+
+# url = gets 
+# client.puts "Your URL #{url}"
+#sputs url
+#url = 'https://medium.com/feed/@olegchursin/'
+
+client.puts "Enter URL"
+url = client.gets.chomp
+client.puts "Your URL #{url}"
+
+open(url) do |rss|
+  feed = RSS::Parser.parse(rss)
+  client.puts "Title: #{feed.channel.title}"
+  feed.items.each do |item|
+    client.puts "Item: #{item.title}"
+  end
+end
+
+
+client.puts "Goodbye #{input}"
+  client.close
+end
+
